3f616db358f65303fa3db57bceb95315ce8e89a5
MarkoAnn <140752687+MarkoAnn@users.noreply.github.com>
2023-07-29 21:52:13 +0300

    newproj

--- auf.go
+++ auf.go
@@ -14,23 +14,21 @@ func MyHandler(w http.ResponseWriter, r *http.Request) {
 func mHandler(w http.ResponseWriter, r *http.Request) {
 	str2 := r.URL.Query().Get("x")
 	str1 := r.URL.Query().Get("y")
-	//str1 := "x"
-	//str2 := "y"
 	number1, err := strconv.Atoi(str1)
 	number2, err1 := strconv.Atoi(str2)
 	if err != nil {
-		//panic(err)
-		fmt.Fprintf(w, "error400")
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "status code:40")
 	}
 	if err1 != nil {
-		//	panic(err1)
-		fmt.Fprintf(w, "error400")
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "status code:40")
+
+	}
+	if err1 == nil {
+		fmt.Fprintf(w, strconv.Itoa(number1*number2))
 	}
 
-	fmt.Fprintf(w, "result = ", number2*number1)
-	if number2*number1 == 0 {
-		fmt.Fprintf(w, "error 400")
-	}
 }
 
 func yHandler(w http.ResponseWriter, r *http.Request) {
